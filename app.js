197cef75af8d798aae7289d306ad0737ae1218cd
Brandon Buchholz <blue.buchholz@gmail.com>
2017-10-12 19:35:47 -0700

    question 4 answered and passes test

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function sum(a,b){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sum(); uncomment it to run it
-//testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -38,7 +38,7 @@ function multiply(a,b){ //eslint-disable-line
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-//testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -74,7 +74,7 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -104,7 +104,7 @@ function sumArray(testArray){ //eslint-disable-line
 //sumArray(testArray);
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
